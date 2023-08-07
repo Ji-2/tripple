951f6e569aac76bd6d53456bb6a17ccff91ae1dc
zltot <wooloim@gmail.com>
2023-08-07 14:40:16 +0900

    Initial commit

---
+++ src/main/java/com/example/tripple/TrippleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrippleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tripple/TrippleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
